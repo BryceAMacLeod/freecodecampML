3992938f777dc95f1397423eaa2267d7e9a5bd2e
BryceAMacLeod <W0436717@nscc.ca>
2022-07-26 15:29:19 -0300

    Core learning algorithms

---
+++ classification.py
@@ -0,0 +1,94 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import tensorflow as tf
+import pandas as pd
+# Where regression was used to predict a numeric value,
+# classification is used to separate data points into
+# classes of different labels. In this example we will use a
+# TensorFlow estimator to classify flowers.
+
+# - data -
+CSV_COLUMN_NAMES = ['SepalLength', 'SepalWidth', 'PetalLength', 'PetalWidth', 'Species']
+SPECIES = ['Setosa', 'Versicolor', 'Virginica']
+# Let's define some constants to help us later on
+train_path = tf.keras.utils.get_file(
+    "iris_training.csv", "https://storage.googleapis.com/download.tensorflow.org/data/iris_training.csv")
+test_path = tf.keras.utils.get_file(
+    "iris_test.csv", "https://storage.googleapis.com/download.tensorflow.org/data/iris_test.csv")
+train = pd.read_csv(train_path, names=CSV_COLUMN_NAMES, header=0)
+test = pd.read_csv(test_path, names=CSV_COLUMN_NAMES, header=0)
+# Here we use keras (a module inside of TensorFlow) to grab our datasets and read them into a pandas dataframe
+train.head()
+# Now we can pop the species column off and use that as our label.
+train_y = train.pop('Species')
+test_y = test.pop('Species')
+train.head()  # the species column is now gone
+print(train.shape)
+
+
+# - input function -
+def input_fn(features, labels, training=True, batch_size=256):
+    # Convert the inputs to a Dataset.
+    dataset = tf.data.Dataset.from_tensor_slices((dict(features), labels))
+
+    # Shuffle and repeat if you are in training mode.
+    if training:
+        dataset = dataset.shuffle(1000).repeat()
+
+    return dataset.batch(batch_size)
+
+
+# Feature columns describe how to use the input.
+my_feature_columns = []
+for key in train.keys():
+    my_feature_columns.append(tf.feature_column.numeric_column(key=key))
+print(my_feature_columns)
+# classification tasks there are variety of different estimators/models that we can pick from.
+# here using DNNClassifier (Deep Neural Network)
+# Build a DNN with 2 hidden layers with 30 and 10 hidden nodes each.
+classifier = tf.estimator.DNNClassifier(
+    feature_columns=my_feature_columns,
+    # Two hidden layers of 30 and 10 nodes respectively.
+    hidden_units=[30, 10],
+    # The model must choose between 3 classes.
+    n_classes=3)
+
+# - training -
+classifier.train(
+    input_fn=lambda: input_fn(train, train_y, training=True),
+    steps=5000)
+# We include a lambda to avoid creating an inner function previously
+
+# - evaluation -
+eval_result = classifier.evaluate(
+    input_fn=lambda: input_fn(test, test_y, training=False))
+
+print('\nTest set accuracy: {accuracy:0.3f}\n'.format(**eval_result))
+
+
+# - predictions -
+def input_fn(features, batch_size=256):
+    # Convert the inputs to a Dataset without labels.
+    return tf.data.Dataset.from_tensor_slices(dict(features)).batch(batch_size)
+
+
+features = ['SepalLength', 'SepalWidth', 'PetalLength', 'PetalWidth']
+predict = {}
+
+print("Please type numeric values as prompted.")
+
+for feature in features:
+    valid = True
+    while valid:
+        val = input(feature + ": ")
+        if not val.isdigit(): valid = False
+
+    predict[feature] = [float(val)]
+
+predictions = classifier.predict(input_fn=lambda: input_fn(predict))
+for pred_dict in predictions:
+    class_id = pred_dict['class_ids'][0]
+    probability = pred_dict['probabilities'][class_id]
+
+    print('Prediction is "{}" ({:.1f}%)'.format(
+        SPECIES[class_id], 100 * probability))
